5930c2ed128d7ed5f24e8710990a9a935d076ce9
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-01 15:38:50 -0800

    Write and test merge

---
+++ sorts/merge.js
@@ -0,0 +1,25 @@
+function mergeSort(arr) {
+    console.log(arr)
+    if (arr.length <= 1) return arr;
+    const midIdx = arr.length / 2;
+
+    const left = arr.slice(0, midIdx);
+    const right = arr.slice(midIdx);
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let sorted = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            sorted.push(left.shift());
+        } else {
+            sorted.push(right.shift());
+        }
+    }
+    return sorted.concat(left, right);
+}
+
+var arr = [5, 4, 3, -5, 2, 8, 1, 10];
+let sorted = mergeSort(arr);
+console.log(sorted);
