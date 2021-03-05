3ae38a77302760301b83a258d3dd7fe7963a1618
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-05 02:01:37 -0800

    Add modal and links to personal

--- src/index.js
+++ src/index.js
@@ -12,6 +12,29 @@ let compared;
 let counter1 = 0;
 let counter2 = 0
 
+// modal logic
+var modal = document.getElementById("myModal");
+var modalBtn = document.getElementById("modal-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+modalBtn.onclick = function () {
+  modal.style.display = "flex";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 // initialize SVG's
 var svg1 = d3
